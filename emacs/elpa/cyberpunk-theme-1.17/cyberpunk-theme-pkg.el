7b67f3f5acaef447af372bda72c572ed3949679e
Marcos García <marcos.g.ciruelos@gmail.com>
2016-06-19 18:49:19 +0200

    Add emacs config files.

---
+++ emacs/elpa/cyberpunk-theme-1.17/cyberpunk-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "cyberpunk-theme" "1.17" "Cyberpunk Color Theme" 'nil :keywords '("color" "theme" "cyberpunk"))
---
+++ emacs/init.el
@@ -0,0 +1,23 @@
+(require 'package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("dcbe22bc74153257f412183dd14ab9652197f59adf65646e618c2577e7cca34d" default)))
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa-stable" . "http://stable.melpa.org/packages/")))))
+(setq linum-format "%4d ")
+(global-linum-mode 1)
+(add-hook 'term-mode-hook (lambda () (linum-mode)))
+(delete-selection-mode 1)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
