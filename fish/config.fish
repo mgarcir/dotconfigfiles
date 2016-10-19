253121865288f304ef3ed50e1eec908d1b5ae115
mgarcia <marcos.garcia@evicertia.com>
2016-10-19 09:51:51 +0200

    Add fish directory.

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+alias gst='git status'
+alias gc='git commit'
+alias gco='git checkout'
+alias gl='git pull'
+alias glg='git log'
+alias gb='git branch'
+alias gmd='git merge-diff'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias dev='cd /cygdrive/d/dev/'
+alias vim='vim -b'
+alias vagssh='vagrant ssh -c "sudo bash"'
+alias vagup="vagrant up"
+alias gmd="git merge-diff"
+alias gmdm="git merge-diff master"
+alias lsa="ls -lart
